380afb602547d13de58fa56d41c8517934ddbd2c
Pablo Pavia <pablopavia118@gmail.com>
2023-02-24 14:57:23 +0100

    portf

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,3 +1,6 @@
-$('.menubutton').click(function(){
-    console.log('click en el menu')
+$('.menu-circle').click(function () {
+  console.log('click en el menu');
+  $('.menu-desplegable').removeClass('blocked');
+  $('.menu-desplegable').addClass('activo');
+  $('menu-desplegable').addClass('animation-menu');
 });
