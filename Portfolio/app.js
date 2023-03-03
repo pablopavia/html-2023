dd2bb4b2a40769fcf63767eb8624ddbc1c095ab8
Pablo Pavia <pablopavia118@gmail.com>
2023-03-03 14:54:46 +0100

    com

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -5,4 +5,25 @@ $('.menu-circle').click(function () {
   $('menu-desplegable').addClass('animation-menu');
 });
 
+$('.image1').click(function () {
+  console.log('click en imagen');
+  $('.imagen-desplegable').removeClass('blocked');
+  $('.imagen-desplegable').addClass('activo');
+  $('.transparent-box').removeClass('blocked');
+  $('.transparent-box').addClass('activo');
+});
 
+$('.transparent-box').click(function () {
+  console.log('click fuera de la imagen');
+  $('.imagen-desplegable').removeClass('activo');
+  $('.imagen-desplegable').addClass('blocked');
+  $('.transparent-box').addClass('blocked');
+  $('.transparent-box').removeClass('activo');
+});
+
+$('.cross-flex').click(function () {
+  $('.imagen-desplegable').removeClass('activo');
+  $('.imagen-desplegable').addClass('blocked');
+  $('.transparent-box').addClass('blocked');
+  $('.transparent-box').removeClass('activo');
+});
